15410b2f76a915d649f8ce5cd9834b49fde209b3
Nataly <97055353+Nataliya1508@users.noreply.github.com>
2022-07-11 00:29:45 +0300

    ...

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,11 @@
-const categoryEl = document.querySelectorAll('.item');
-const numberCategoryEl = categoryEl.length;
-console.log('Number of categories:', numberCategoryEl);
+// const categoryEl = document.querySelectorAll('.item');
+// console.log(categoryEl);
+// const numberCategoryEl = categoryEl.length;
+// console.log('Number of categories:', numberCategoryEl);
+
+// categoryUl.forEach(fanction(el, index) {
+//     const numberOfElements = el.querySelectorAll('li');
+//     const nameOfElements = el.querySelector('h2');
+//     console.log('Category: ${nameOfElements.textContent}');
+
+// })
