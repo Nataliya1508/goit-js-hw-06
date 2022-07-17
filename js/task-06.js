38c36de2c29d0d120764d11eb2456483bd943dd0
Nataly <97055353+Nataliya1508@users.noreply.github.com>
2022-07-17 22:57:14 +0300

    edits (task 6, task 8)

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const input = document.querySelector('input');
 const validLength = parseInt(input.getAttribute("data-length"));
 
 input.addEventListener('blur', (event) => {
-    if (event.currentTarget.value.length <= validLength) {
+    if (event.currentTarget.value.length === validLength) {
         event.currentTarget.classList.add('valid')
         event.currentTarget.classList.remove('invalid')
     }
--- js/task-08.js
+++ js/task-08.js
@@ -2,25 +2,21 @@ const form = document.querySelector('.login-form')
 
 form.addEventListener('submit', onLoginForm)
 
+
+
 function onLoginForm(event) {
-    event.preventDefault();
-    const formElements = event.currentTarget.elements;
-    const email = formElements.email
-    const password = formElements.password;
-
-    const formData = {
-        email: email.value,
-        password: password.value,
-    }
-    
-    console.log(formData);
-    form.reset();
-
-    if (email === "" || password === "") {
-        alert('Все поля должны быть заполнены');
-        form.reset();
-       return
-    }
-  
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  if (email === '' || password === '') {
+    alert('Все поля должны быть заполнены');
+    return;
+  }
+  const formData = {
+    email,
+    password,
+  };
+  console.log(formData);
+  form.reset();
 }
-
